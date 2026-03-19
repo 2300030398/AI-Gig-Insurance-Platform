3c01c2f32cb8d5fc9f4ec170bb82278887f0f994
MAJJIGA V ASHIKA REDDY <163742933+2300030398@users.noreply.github.com>
2026-03-19 23:38:06 +0530

    Add user registration and policy functions

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function register() {
+    alert("User Registered!");
+}
+
+function buyPolicy() {
+    alert("Policy Activated (₹49/week)");
+}
+
+function triggerRain() {
+    document.getElementById("status").innerText =
+        "Heavy rain detected → Claim triggered → ₹500 payout!";
+}
