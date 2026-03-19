d73f063606b059cf89ea8b982c0faed90aa03e51
MAJJIGA V ASHIKA REDDY <163742933+2300030398@users.noreply.github.com>
2026-03-19 23:48:26 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,12 @@
 function register() {
-    alert("User Registered Successfully!");
+    let name = document.getElementById("name").value;
+    let city = document.getElementById("city").value;
+
+    if (name === "" || city === "") {
+        alert("Please enter Name and City");
+    } else {
+        alert("User Registered: " + name + " (" + city + ")");
+    }
 }
 
 function buyPolicy() {
